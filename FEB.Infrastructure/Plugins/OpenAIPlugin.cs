22bff275ab833be852ba6b8f89a490253e705c50
burack <omerfaruk2695@gmail.com>
2025-04-19 00:43:49 +0200

    [UPDATE] PLUGIN

--- FEB.Infrastructure/Plugins/OpenAIPlugin.cs
+++ FEB.Infrastructure/Plugins/OpenAIPlugin.cs
@@ -16,5 +16,37 @@ namespace FEB.Service.Plugins
         {
             return DateTime.Now;
         }
+
+
+        [KernelFunction]
+        [Description("Returns the current user information Async.")]
+        public async Task<UserInfo> GetCurrentUserInformation()
+        {
+            return await this.GetUserInfoAsync() ;
+        }
+        [KernelFunction]
+        [Description("Creates an appointment for the current user. Automatically uses GetCurrentUserInformation to get the username. Does not require a date or time.")]
+        public bool CreateAppointmentByUserName(string username)
+        {
+            var d = username.ToUpper();
+            return true;
+        }
+        public struct UserInfo
+        {
+            public string UserName { get; set; }
+            public string UserEmail { get; set; }
+            public string UserRole { get; set; }
+        }
+
+        public async Task<UserInfo> GetUserInfoAsync()
+        {
+            var info = new UserInfo
+            {
+                UserEmail = "Omerfaruk2695@gmail.com",
+                UserName = "burack18",
+                UserRole = "Admin"
+            };
+            return await Task.FromResult(info);
+        }
     }
 }
